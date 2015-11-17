e7a06268239a9abf1accfef9fbb37c8d77f9b1d7
mclytus <mclytus@gmail.com>
2015-11-17 13:27:34 -0800

    Update owl2_chado.php

--- owl2_chado.php
+++ owl2_chado.php
@@ -1,1 +1,22 @@
+<?php
+/**
+ *  @file
+ *  @add file from header
+ *  
+*/
 
+$value = 
+
+$sectionheader ["name"] = 'owl:Ontology';
+$sectionheader ["attribute"] = array ($key-> value);
+$sectionheader ["children"] = array();
+	$sectionheader ["children"][]['name'] = 'foaf:homepage';
+	
+// load the XML file
+$owl = new XMLReader();
+
+// We are going to go through the owl header section to get through the tags.
+$owl->open($sectionheader);
+while ($sectionheader->read()){                                                           
+	
+}
