16a529a028f1090e404780bfd53d926138571742
mclytus <mclytus@gmail.com>
2015-11-17 15:03:39 -0800

    Update owl2_chado.php

--- owl2_chado.php
+++ owl2_chado.php
@@ -5,18 +5,23 @@
  *  
 */
 
-$value = 
+$value = //URI rdf:about = ""
 
 $sectionheader ["name"] = 'owl:Ontology';
 $sectionheader ["attribute"] = array ($key-> value);
 $sectionheader ["children"] = array();
 	$sectionheader ["children"][]['name'] = 'foaf:homepage';
+	$sectionheader ["children"][]['attribute'] = 'http://obo-relation.googlecode.com';
+	$sectionheader ["children"][]['value'] = 
+	
 	
 // load the XML file
 $owl = new XMLReader();
 
 // We are going to go through the owl header section to get through the tags.
 $owl->open($sectionheader);
-while ($sectionheader->read()){                                                           
-	
+$total_nodes =0;
+while ($sectionheader->read()){  
+	$total_nodes++;
 }
+$owl->close();
